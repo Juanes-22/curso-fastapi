1150acbeadf7e751d19fd78dbd3ba329703ab774
Juan Esteban García Martínez <juan.garcia@premexcorp.com>
2025-01-02 11:04:29 -0500

    Update project structure

--- clase-1-11/constants.py
+++
@@ -1,7 +0,0 @@
-COUNTRY_TIMEZONES = {
-    "CO": "America/Bogota",
-    "MX": "America/Mexico_City",
-    "AR": "America/Argentina/Buenos_Aires",
-    "BR": "America/Sao_Paulo",
-    "PE": "America/Lima",
-}
